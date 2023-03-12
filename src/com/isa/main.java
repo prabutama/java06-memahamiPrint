0b18826196a0e2d90f6a35c0101be044e48c46b8
prabutama <isaprabutama@gmail.com>
2023-03-13 05:16:16 +0700

    Initial commit

---
+++ src/com/isa/main.java
@@ -0,0 +1,12 @@
+package com.isa;
+
+public class main {
+    public static void main (String[] args) {
+
+        System.out.print(1 +" "+ "hallo aku isa"); // "print" display = inline
+
+        System.out.println(" "+" "+ "ayo bermain"); // "println" display = block
+
+        System.out.println(2 +" "+ "ayo ");  // "println" display = block
+    };
+};
